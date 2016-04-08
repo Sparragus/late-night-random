0c46b0eb2a814d0cb7ec0815951b5cfd65f3d78a
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-08 15:52:57 -0500

    Refactors code and sends message on channel open

--- src/index.js
+++ src/index.js
@@ -10,33 +10,44 @@ function parseTime (time) {
 }
 
 const openingTime = parseTime(config.opening_time)
-const openChannelJob = new CronJob(`00 ${openingTime.minute} ${openingTime.hour} * * *`, async function openChannel () {
-  console.log('Opening the channel')
-  // Check if channel exists. If not, create it
-  let channel = await slack.getChannelByName(config.channel_name)
-  if (!channel) {
-    channel = (await slack.createChannel(config.channel_name)).channel
-  }
+const openChannelJob = new CronJob({
+  cronTime: `00 ${openingTime.minute} ${openingTime.hour} * * *`,
+  onTick: async function openChannel () {
+    console.log('Opening the channel')
+    // Check if channel exists. If not, create it
+    let channel = await slack.getChannelByName(config.channel_name)
+    if (!channel) {
+      channel = (await slack.createChannel(config.channel_name)).channel
+    }
 
-  // If the channel is archived, open it again.
-  if (channel.is_archived) {
-    await slack.unarchiveChannelByName(config.channel_name)
-  }
-}, null, true)
+    // If the channel is archived, open it again.
+    if (channel.is_archived) {
+      await slack.unarchiveChannelByName(config.channel_name)
+    }
+
+    slack.sendMessageToChannelByName('random', '@here: #latenightrandom is open!', {parse: 'full'})
+  },
+  start: true,
+  timeZone: config.timezone
+})
 
 const closingTime = parseTime(config.closing_time)
-const closeChannelJob = new CronJob(`00 ${closingTime.minute} ${closingTime.hour} * * *`, async function closeChannel () {
-  console.log('Closing the channel')
-  // If the channel doesn't exist, do nothing
-  let channel = await slack.getChannelByName(config.channel_name)
-  if (!channel) {
-    return
-  }
+const closeChannelJob = new CronJob({
+  cronTime: `00 ${closingTime.minute} ${closingTime.hour} * * *`,
+  onTick: async function closeChannel () {
+    console.log('Closing the channel')
+    // If the channel doesn't exist, do nothing
+    let channel = await slack.getChannelByName(config.channel_name)
+    if (!channel) {
+      return
+    }
 
-  // Delete all the messages
-  await slack.deleteAllMessagesFromChannelByName(config.channel_name)
+    // Delete all the messages
+    await slack.deleteAllMessagesFromChannelByName(config.channel_name)
 
-  // Archive the channel
-  await slack.archiveChannelByName(config.channel_name)
-}, null, true)
-
+    // Archive the channel
+    await slack.archiveChannelByName(config.channel_name)
+  },
+  start: true,
+  timeZone: config.timezone
+})
