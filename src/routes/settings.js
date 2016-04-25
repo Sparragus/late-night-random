e21ea4bfd636b471ebe89bb12ead4c9f99b828f4
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-25 12:29:01 -0500

    Adds feature to update timezone

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,7 +1,35 @@
+import Team from '../models/team'
+
 export async function get (ctx, next) {
-  await ctx.render('settings', {})
+  if (!ctx.session || !ctx.session.teamId) {
+    // TODO: Error flash message
+    return ctx.redirect('/')
+  }
+
+  const team = await Team.findOne({id: ctx.session.teamId}).exec()
+  await ctx.render('settings', {
+    timezone: team.timezone
+  })
 }
 
-export function update (ctx, next) {
+export async function update (ctx, next) {
+  if (!ctx.session || !ctx.session.teamId) {
+    // TODO: Error flash message
+    return ctx.redirect('/')
+  }
 
+  const { timezone } = ctx.request.body
+  if (timezone !== '') {
+    await Team.findOneAndUpdate(
+      {id: ctx.session.teamId},
+      {
+        timezone
+      },
+      {
+        new: true
+      }
+    ).exec()
+  }
+
+  return ctx.redirect('/settings')
 }
