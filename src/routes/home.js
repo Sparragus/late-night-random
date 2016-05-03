72b7671c16a3308900fb11cb9f915a9f936259f6
Richard Kaufman <richardbkaufman@gmail.com>
2016-05-02 22:17:01 -0500

    Adds flash message

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,6 +1,10 @@
 export default async function home (ctx, next) {
+  const flash = {...ctx.session.flash}
+  ctx.session.flash = {}
+
   const locals = {
-    appInstalled: !!ctx.session.teamId
+    appInstalled: !!ctx.session.teamId,
+    flash
   }
 
   await ctx.render('index', locals)
