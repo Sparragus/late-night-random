e9a10ba4089eb62f7a11dbe7972c217ae9be1a0c
Richard Kaufman <richardbkaufman@gmail.com>
2016-05-04 18:53:11 -0500

    Removes commands

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import compose from 'koa-compose'
 import oauth from './oauth'
 import home from './home'
 import * as settings from './settings'
-import command from './command'
 
 export default function () {
   const router = new Router()
@@ -14,7 +13,6 @@ export default function () {
   router.get('/logout', (ctx, next) => { ctx.session = null; ctx.redirect('/') })
   router.get('/settings', settings.get)
   router.post('/settings', settings.update)
-  router.post('/command', command)
 
   return compose([
     router.routes(),
